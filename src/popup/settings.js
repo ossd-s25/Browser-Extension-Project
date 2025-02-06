0845a4cd8293ba97c607f1730317a8562ae9a869
yd2960 <yd2960@nyu.edu>
2025-02-06 16:04:34 -0500

    additional code comments

--- src/popup/settings.js
+++ src/popup/settings.js
@@ -1,3 +1,4 @@
+// read and initialize slider value
 browser.storage.local.get("endless_scroll").then((result) => {
     const checkbox = document.getElementById("checkbox");
     if (result.endless_scroll) {
@@ -7,10 +8,11 @@ browser.storage.local.get("endless_scroll").then((result) => {
     }
 });
 
+// wait for elements to load (not really needed)
 document.addEventListener("DOMContentLoaded", () => {
     const checkbox = document.getElementById("checkbox");
 
-    // Attach the event listener
+    // Attach the event listener to record slider
     checkbox.addEventListener("change", () => {
         browser.storage.local.set({ endless_scroll: checkbox.checked });
     });
--- src/scroll.js
+++ src/scroll.js
@@ -1,8 +1,11 @@
+// Returns true if the page is scrolled all the way to the bottom
 function isScrolledToBottom() {
     const threshold = 5; // px
     return window.innerHeight + window.scrollY >= document.body.scrollHeight - threshold;
 }
 
+
+// Duplicate all elements betwen the body tag and append them to the body tag
 function duplicatePageContent() {
     const clonedBodyChildren = Array.from(document.body.childNodes).map(child => child.cloneNode(true));
     clonedBodyChildren.forEach(node => {
@@ -10,6 +13,7 @@ function duplicatePageContent() {
     });
 }
 
+// variables to prevent multiple duplication in one scroll instance
 let throttled = false;
 const throttleDuration = 500; // ms
 
@@ -17,7 +21,7 @@ const throttleDuration = 500; // ms
 window.addEventListener("wheel", (event) => {
     if (throttled || !event.deltaY)
         return;
-    // Read setting
+    // Read setting from slider
     browser.storage.local.get("endless_scroll").then((result) => {
         if (isScrolledToBottom() && result.endless_scroll) {
             duplicatePageContent();
